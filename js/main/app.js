37e7e8f1a8722e6014a3d2d2f66c54a841262466
will <will@ec-media.com.tw>
2021-03-05 16:51:35 +0800

    動畫測試v01

---
+++ js/main/app.js
@@ -0,0 +1,111 @@
+//場景
+function createScene() {
+  const scene = new THREE.Scene();
+  scene.background = new THREE.Color(0xffffff);
+  return scene;
+}
+
+//相機
+function createCamera() {
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.z = 5;
+  return camera;
+}
+
+//燈光
+function createLight() {
+  const light = new THREE.PointLight(0xffffff, 1, 1000);
+  light.position.set(0, 0, 10);
+  return light;
+}
+
+//3D幾何物件
+function createCube({ color, x, y }) {
+  const geometry = new THREE.BoxGeometry();
+  const material = new THREE.MeshLambertMaterial({ color });
+  const cube = new THREE.Mesh(geometry, material);
+  cube.position.set(x, y, 0);
+
+  return cube;
+}
+
+//渲染到畫面
+function createRenderer() {
+  const root = document.getElementById("app");
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  root.appendChild(renderer.domElement);
+  return renderer;
+}
+
+//動畫執行
+function animate(callback) {
+  function loop(time) {
+    callback(time);
+    requestAnimationFrame(loop);
+  }
+  requestAnimationFrame(loop);
+}
+
+const renderer = createRenderer();
+const scene = createScene();
+const camera = createCamera();
+const interactionManager = new THREE.InteractionManager(
+  renderer,
+  camera,
+  renderer.domElement
+);
+
+const cubes = {
+  pink: createCube({ color: 0xff00ce, x: -1, y: -1 }),
+  purple: createCube({ color: 0x9300fb, x: 1, y: -1 }),
+  blue: createCube({ color: 0x0065d9, x: 1, y: 1 }),
+  cyan: createCube({ color: 0x00d7d0, x: -1, y: 1 }),
+};
+
+const light = createLight();
+
+for (const [name, object] of Object.entries(cubes)) {
+  object.addEventListener("click", (event) => {
+    event.stopPropagation();
+    console.log(`${name} cube was clicked`);
+    const cube = event.target;
+    const coords = { x: camera.position.x, y: camera.position.y };
+    const tween = new TWEEN.Tween(coords)
+      .to({ x: cube.position.x, y: cube.position.y })
+      .easing(TWEEN.Easing.Quadratic.Out)
+      .onUpdate(() =>
+        camera.position.set(coords.x, coords.y, camera.position.z)
+      )
+      .start();
+  });
+  interactionManager.add(object);
+  scene.add(object);
+}
+
+//RWD響應
+window.addEventListener("resize", onWindowResize, false);
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+//相機控制
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
+// controls.target.set(0, 100, 0);
+controls.update();
+
+scene.add(light);
+
+animate((time) => {
+  renderer.render(scene, camera);
+  interactionManager.update();
+  TWEEN.update(time);
+});
